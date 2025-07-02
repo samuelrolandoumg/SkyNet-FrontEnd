d1a3d07cf3af52c5e10683fa7018e4be24d5fef6
Samuel Roland <samuelrolandoumg@gmail.com>
2025-07-01 18:19:09 -0600

    fix: corregido server.js para apuntar a carpeta 'browser'

--- server.js
+++ server.js
@@ -1,18 +1,17 @@
 const express = require('express');
 const path = require('path');
+
 const app = express();
 
-const PORT = process.env.PORT || 3000;
-
-
-const DIST_FOLDER = path.join(__dirname, 'dist/SkyNet_desa_f/browser');
-
-app.use(express.static(DIST_FOLDER));
+// Cambia la ruta al subdirectorio browser
+app.use(express.static(path.join(__dirname, 'dist/SkyNet_desa_f/browser')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(DIST_FOLDER, 'index.html'));
+    res.sendFile(path.join(__dirname, 'dist/SkyNet_desa_f/browser/index.html'));
 });
 
+// Configuración del puerto
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-  console.log(`Servidor corriendo en el puerto ${PORT}`);
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
