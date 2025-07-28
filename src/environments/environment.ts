ab75687f9e4840be17c28cc0e2b0b946ef484646
Samuel Roland <samuelrolandoumg@gmail.com>
2025-07-27 21:45:06 -0600

    estado con incidenca

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 // src/environments/environment.ts
 export const environment = {
     production: false,
-    apiUrl: 'https://skynet-backend-production.up.railway.app'
-    //apiUrl: 'http://localhost:8080'
+    //apiUrl: 'https://skynet-backend-production.up.railway.app'
+    apiUrl: 'http://localhost:8080'
 };
